bff51f37a06e5b29818923aea2cd47a1fa9e0d9b
EliasBautista <66222438+EliasBautista@users.noreply.github.com>
2020-10-21 19:40:59 -0500

    POST en pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,14 +3,27 @@ const pokemon = express.Router();
 //const pk = require('../pokedex.json').pokemon;
 const db = require('../config/database');
 
-pokemon.post("/", (req, res, next) =>{
-    return res.status(200).send(req.body);
+pokemon.post("/", async (req, res, next) =>{
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += `VALUES ('${pok_name}',${pok_height},${pok_weight},${pok_base_experience})`;
+        const rows = await db.query(query);
+        //console.log(rows);
+
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({code: 201, message: "Pokemon Insertado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos Incompletos"});
 });
 
 pokemon.get("/", async (req, res, next) =>{
     const pkmn = await db.query("SELECT * FROM POKEMON");
     //console.log(pkmn);
-    return res.status(200).json({code: 1, message: pkmn});
+    return res.status(200).json({code: 200, message: pkmn});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
@@ -20,7 +33,7 @@ pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     if (Object.entries(pkmn).length === 0){
         return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
     }else{
-        return res.status(200).json({code: 1, message: pkmn});
+        return res.status(200).json({code: 200, message: pkmn});
     }
     // if (id >= 0 && id <= 150) {
     //     return res.status(200).send(pk[req.params.id - 1]);
@@ -34,7 +47,7 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     if (Object.entries(pkmn).length === 0){
         return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
     }else{
-        return res.status(200).json({code: 1, message: pkmn});
+        return res.status(200).json({code: 200, message: pkmn});
     }
     /* const pkmn = pk.filter((p) =>{
         return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
