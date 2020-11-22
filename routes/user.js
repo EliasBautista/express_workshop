446dcda313af66bf333667505077930e511357a8
EliasBautista <66222438+EliasBautista@users.noreply.github.com>
2020-11-21 19:28:58 -0600

    Integrando con la UI2

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ user.post("/signin", async(req, res, next) =>{
 
     if(user_name && user_mail && user_password){
         let query = "INSERT INTO user (user_name, user_mail, user_password) ";
-        query += `VALUES('${user_name}', '${user_mail}', '${user_password})')`;
+        query += `VALUES('${user_name}', '${user_mail}', '${user_password}')`;
 
         const rows = await db.query(query);
 
@@ -34,7 +34,7 @@ user.post("/login", async(req, res, next)=> {
             }, "debugkey");
             return res.status(200).json({code:200, message:token});
         }else{
-            return res.status(200).json({code:200, message:"Usuario y/o contraseña incorrectos"});
+            return res.status(200).json({code:401, message:"Usuario y/o contraseña incorrectos"});
         }
     }
     return res.status(500).json({code:500, message:"Campos incompletos"});
